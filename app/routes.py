e4a92baa60d8b728ee24893423e9be490620690f
sfy2008 <sfy20081@qq.com>
2021-11-06 19:20:43 +0800

    just_follow_and_test

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
+from flask import render_template
+
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world! "
+    user = {'username':"robinson"}
+    return render_template('index.html',title='home',user= user)
